24513a9d2ec36840df3c6881df917963dd2a9379
78Hypnotic <manuel.hohlwegler@gmx.de>
2024-08-22 17:47:42 +0200

    ui überarbeitet

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,4 @@
-"<template>
+<template>
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
@@ -14,35 +14,43 @@
           <ion-select-option value="mlkit">ML Kit</ion-select-option>
         </ion-select>
       </ion-item>
-      
-      <ion-item>
-        <ion-label position="floating"><strong>Eingabesprache</strong></ion-label>
-        <ion-select v-model="sourceLanguage">
-          <ion-select-option value="de">Deutsch</ion-select-option>
-          <ion-select-option value="en">Englisch</ion-select-option>
-          <ion-select-option value="fr">Französisch</ion-select-option>
-          <ion-select-option value="it">Italienisch</ion-select-option>
-          <ion-select-option value="es">Spanisch</ion-select-option>
-        </ion-select>
-      </ion-item>
+      <ion-grid>
+        <ion-row class="ion-align-items-center">
+          <ion-col size="5">
+            <ion-item>
+              <ion-select v-model="sourceLanguage" interface="popover" class="custom-language-select">
+                <ion-select-option value="de">Deutsch</ion-select-option>
+                <ion-select-option value="en">Englisch</ion-select-option>
+                <ion-select-option value="fr">Französisch</ion-select-option>
+                <ion-select-option value="it">Italienisch</ion-select-option>
+                <ion-select-option value="es">Spanisch</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </ion-col>
 
-      <ion-item>
-        <ion-label position="floating"><strong>Ausgabesprache</strong></ion-label>
-        <ion-select v-model="targetLanguage">
-          <ion-select-option value="de">Deutsch</ion-select-option>
-          <ion-select-option value="en">Englisch</ion-select-option>
-          <ion-select-option value="fr">Französisch</ion-select-option>
-          <ion-select-option value="it">Italienisch</ion-select-option>
-          <ion-select-option value="es">Spanisch</ion-select-option>
-        </ion-select>
-      </ion-item>
+          <ion-col size="2" class="ion-text-center">
+            <ion-icon class="swap-icon" :icon="swapHorizontalOutline" @click="swapLanguages"></ion-icon>
+          </ion-col>
+
+          <ion-col size="5">
+            <ion-item>
+              <ion-select v-model="targetLanguage" interface="popover" class="custom-language-select">
+                <ion-select-option value="de">Deutsch</ion-select-option>
+                <ion-select-option value="en">Englisch</ion-select-option>
+                <ion-select-option value="fr">Französisch</ion-select-option>
+                <ion-select-option value="it">Italienisch</ion-select-option>
+                <ion-select-option value="es">Spanisch</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
       <ion-item>
         <ion-textarea :rows="6" v-model="sourceText" placeholder="Gib hier den zu übersetzenden Text ein..."></ion-textarea>
       </ion-item>
 
-      <ion-button expand="block" @click="translateText">Übersetzen</ion-button>
-      <ion-button expand="block" @click="swapLanguages">Sprachen tauschen</ion-button>
+      <ion-button class="rounded-button" expand="block" @click="translateText">Übersetzen</ion-button>
 
       <ion-card>
         <ion-card-header>
@@ -53,30 +61,40 @@
         </ion-card-content>
       </ion-card>
 
-      <ion-button expand="block" @click="copyText">In Zwischenablage kopieren</ion-button>
-      <ion-button expand="block" @click="speakText">Vorlesen</ion-button>
+      <ion-grid>
+        <ion-row class="ion-justify-content-center">
+          <ion-col size="auto">
+            <ion-button class="icon-button" @click="copyText">
+              <ion-icon :icon="clipboard"></ion-icon>
+            </ion-button>
+          </ion-col>
+          <ion-col size="auto">
+            <ion-button class="icon-button" @click="speakText">
+              <ion-icon :icon="volumeHigh"></ion-icon>
+            </ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
       <ion-toast :is-open="showToast" :message="toastMessage" :duration="3000"></ion-toast>
 
-      <ion-loading
-        :is-open="loading"
-        message="Downloading language model..."
-      ></ion-loading>
+      <ion-loading :is-open="loading" message="Downloading language model..."></ion-loading>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonSelect, IonSelectOption, IonTextarea, IonButton, IonCard, IonCardHeader, IonCardContent, IonToast, IonLoading, alertController } from '@ionic/vue';
+import { IonContent, IonCol, IonGrid, IonRow, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonSelect, IonSelectOption, IonTextarea, IonButton, IonCard, IonCardHeader, IonCardContent, IonToast, IonLoading, alertController, IonIcon } from '@ionic/vue';
 import { ref, onMounted } from 'vue';
 import { Network } from '@capacitor/network';
 import { Clipboard } from '@capacitor/clipboard';
 import { TextToSpeech } from '@capacitor-community/text-to-speech';
 import { Translation, Language } from '@capacitor-mlkit/translation';
 import axios from 'axios';
+import { clipboard, swapHorizontalOutline, volumeHigh } from 'ionicons/icons';
 
-const sourceLanguage = ref(Language.English); // Using enum from ML Kit
-const targetLanguage = ref(Language.German); // Using enum from ML Kit
+const sourceLanguage = ref(Language.English);
+const targetLanguage = ref(Language.German);
 const sourceText = ref('');
 const translatedText = ref('');
 const showToast = ref(false);
@@ -149,13 +167,11 @@ async function confirmDownloadOnMobileData() {
   return result.role === 'continue';
 }
 
-// Utility function to swap source and target languages
 function swapLanguages() {
   [sourceLanguage.value, targetLanguage.value] = [targetLanguage.value, sourceLanguage.value];
   [sourceText.value, translatedText.value] = [translatedText.value, sourceText.value];
 }
 
-// Function to copy translated text to the clipboard
 async function copyText() {
   try {
     await Clipboard.write({
@@ -169,7 +185,6 @@ async function copyText() {
   }
 }
 
-// Function to speak the translated text
 async function speakText() {
   try {
     await TextToSpeech.speak({
@@ -181,7 +196,6 @@ async function speakText() {
   }
 }
 
-//This hook preloads the English model if ML Kit is chosen as the translation method
 onMounted(async () => {
   if (translationMethod.value === 'mlkit') {
     loading.value = true;
@@ -189,5 +203,49 @@ onMounted(async () => {
     loading.value = false;
   }
 });
-
 </script>
+
+<style scoped>
+.rounded-button {
+  --border-radius: 8px;
+  --background: #4d4d4d8f;
+  width: 50%;
+  margin: 20px auto;
+}
+
+.swap-icon {
+  font-size: 24px;
+  color: #000;
+  cursor: pointer;
+}
+
+.custom-language-select {
+  --padding-start: 10px;
+  --padding-end: 10px;
+  --background: #e0e0e0;
+  --border-radius: 8px;
+  --placeholder-color: #6d6d6d;
+  --text-align: center; /* Center text alignment */
+}
+
+.ion-justify-content-center {
+  justify-content: center;
+}
+
+.icon-button {
+  --border-radius: 8px;
+  --background: #4d4d4d8f;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 8px;
+  --padding: 0;
+}
+
+.icon-button ion-icon {
+  font-size: 24px;
+  color: #fff;
+}
+</style>
